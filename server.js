01017feb53fb67154fed1bc4516ebfcf4a4352bc
Mayaiskool5 <104118878+Mayaiskool5@users.noreply.github.com>
2026-02-24 10:41:15 -0500

    car dealer start

---
+++ server.js
@@ -0,0 +1,12 @@
+
+
+
+
+/**
+ * Start Server
+ */
+app.listen(PORT, async () => {
+    await setupDatabase();
+    await testConnection();
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
