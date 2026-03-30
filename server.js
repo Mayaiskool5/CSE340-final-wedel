d1ff2f7711fc5e94d22ab2e69f383107e0910984
Mayaiskool5 <104118878+Mayaiskool5@users.noreply.github.com>
2026-03-30 11:43:44 -0400

    updates

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: NODE_ENV.includes('dev') !== true,
+        secure: NODE_ENV === 'production',
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000 // 24 Hours
     }
